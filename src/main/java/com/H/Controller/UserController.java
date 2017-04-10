c9905d477407b04e7dc263cf9784146b4b559ce7
silly-lone-wolf <3297449167@qq.com>
2017-04-10 20:55:14 +0800

    1211

---
+++ src/main/java/com/H/Controller/UserController.java
@@ -0,0 +1,27 @@
+package com.H.Controller;
+
+import com.H.Controller.SysUserRepository;
+import com.H.Modle.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by dell on 2017/4/9.
+ */
+@RestController
+public class UserController {
+
+    @Autowired
+    SysUserRepository userRepository;
+    @GetMapping("/user/add")
+    public User add() {
+        return userRepository.findByName("zhang");
+    }
+
+    @RequestMapping("/user/update")
+    public String update() {
+        return "update";
+    }
+}
---
+++ src/main/java/com/H/WebSecurityConfiguration.java
@@ -0,0 +1,49 @@
+package com.H;
+
+import com.H.Modle.RestAuthenticationFailureHandler;
+import com.H.Modle.RestAuthenticationSuccessHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * Created by dell on 2017/4/9.
+ */
+//安全框架security的配置类
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private RestAuthenticationFailureHandler authenticationFailureHandler;
+    @Autowired
+    private RestAuthenticationSuccessHandler successHandler;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();// 关闭csrf:Cross-site request forgery跨站请求伪造（便于测试post请求）
+        //http.authorizeRequests().anyRequest().authenticated();//所有页面访问都需要先登录
+        http.formLogin() // 登陆的设置
+                .failureHandler(authenticationFailureHandler) // failure handler登录失败返回结果
+                .successHandler(successHandler) // success handler
+                //.loginPage("loginPage")  //使用的自定义登录页面
+                .loginProcessingUrl("/login")//登录请求的url  default is /login with an HTTP post
+                //.defaultSuccessUrl("/user/add")//登陆成功后默认的登录跳转页面
+                .permitAll(); // premit all authority登录页面允许所有人访问
+        http.authorizeRequests().antMatchers("/user/*").hasAnyAuthority("USER");//拦截指定url,放行指定角色
+        //http.authorizeRequests().antMatchers("/log").permitAll();//允许哪些页面对所有人开放
+        }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);//将userdetailsserviceimple设置为验证用户信息的类。
+        // auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");//创建一个默认的用户存储在内存中
+    }
+}
